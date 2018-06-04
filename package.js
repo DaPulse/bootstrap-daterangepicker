1a07635f770bb6c8265c2dee81c378101a138625
Daniel B <danielbb124@gmail.com>
2018-06-04 15:41:45 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: 'dangrossman:bootstrap-daterangepicker',
+  name: 'bladepop:bootstrap-daterangepicker',
   version: '3.0.3',
   summary: 'Date range picker component',
   git: 'https://github.com/dangrossman/daterangepicker',
